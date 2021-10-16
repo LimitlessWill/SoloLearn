fae32f758d5f9b057668970c6fc344aae96846f7
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-10-16 05:28:11 +0200

    Create Popsicles.java

---
+++ Popsicles/Popsicles.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Program
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int siblings = input.nextInt();
+        int popsicles = input.nextInt();
+        
+        //your code goes here
+       
+if((popsicles % siblings )==0)
+       System.out.println("give away");
+       else
+       System.out.println("eat them yourself");
+    }
+}
