5aed2ecffdc892994d1553ebbda6c8881c8c7743
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-10-16 23:06:42 +0200

    Create Fruit_Bowl.java

---
+++ Fruit_Bowl/Fruit_Bowl.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Program
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int fruit = input.nextInt();
+        
+        //your code goes here
+       
+System.out.println( (fruit/2)/3);
+    }
+}
