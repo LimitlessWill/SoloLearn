27a1f7025152f1c47c192c0de00a2cdfe7e36559
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-10-16 23:04:48 +0200

    Create Fruit_Bowl.cpp

---
+++ Fruit_Bowl/Fruit_Bowl.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int fruit;
+    //take input
+    cin>>fruit;
+
+    //your code goes here
+    
+cout<<(fruit/2)/3;
+    return 0;
+}
