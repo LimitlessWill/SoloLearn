569d5990d09c8e35db62d74a18f851ec1c891878
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-10-16 23:05:48 +0200

    Create Fruit_Bowl.cs

---
+++ Fruit_Bowl/Fruit_Bowl.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fruit;
+            fruit = Convert.ToInt32(Console.ReadLine());
+            
+            //your code goes here
+            
+Console.WriteLine( (fruit/2)/3);
+        }
+    }
+}
