3d4775b10b300d1e6f05098b4a9a0c361c32aff8
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-10-16 05:49:29 +0200

    Create Halloween_Candy.cpp

---
+++ Halloween_Candy/Halloween_Candy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int houses;
+    cin>>houses;
+
+    //your code goes here
+    
+cout<<ceil((2.0/houses)*100);
+    return 0;
+}
