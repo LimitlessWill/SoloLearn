eb1552c97911dac481a886b1fb5a14ad0a9dfa9c
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-10-17 05:13:52 +0200

    Create Gotham_City.cpp

---
+++ Gotham_City/Gotham_City.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+unsigned cr;
+cin>>cr;
+if(cr < 5)
+cout<<"I got this!";
+else if (cr >4 && cr < 11)
+cout<<("Help me Batman");
+else
+cout<<("Good Luck out there!");
+
+    return 0;
+}
