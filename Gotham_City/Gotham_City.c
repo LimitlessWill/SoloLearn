1c3e7585ae67a58920870ef5737e3d8f13e1d3ab
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-10-17 05:13:16 +0200

    Create Gotham_City.c

---
+++ Gotham_City/Gotham_City.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+unsigned cr;
+scanf("%d",&cr);
+if(cr < 5)
+printf("I got this!");
+else if (cr >4 && cr < 11)
+printf("Help me Batman");
+else
+printf("Good Luck out there!");
+    return 0;
+}
