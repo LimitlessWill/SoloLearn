d5d62fc4c167e542b94921d61104593ec2a65508
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-10-25 21:07:47 +0200

    Update No_Numerals.py
    - Increase readability

--- No_Numerals/No_Numerals.py
+++ No_Numerals/No_Numerals.py
@@ -1,6 +1,9 @@
 msg = input().split()
 res = ""
 nums = {"0":"zero","1":"one","2":"two","3":"three","4":"four","5":"five","6":"six","7":"seven","8":"eight","9":"nine","10":"ten"}
+
 for x in msg:
-    res += x+" " if x not in nums else nums[x]+" "
+    res += nums[x] if x in nums else x
+    res += " "
+
 print(res,end="")
